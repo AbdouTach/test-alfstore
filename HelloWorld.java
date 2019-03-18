ffb4959ffecbae5553dea81586ad257649a79b1f
Abdelillah TACHTIOUI <abdelillah.tachtioui@gmail.com>
2019-03-18 17:35:56 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package alfstore;
+public class HelloWorld
+{   
+      public static void main(String[] args)
+      {       
+         System.out.println("Hello alfstore @ GitHub!");   
+      }
+} 
+
