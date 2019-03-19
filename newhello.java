8b6d46213e4a87cd226c6631f2079fc333ea0346
Abdelillah TACHTIOUI <abdelillah.tachtioui@gmail.com>
2019-03-19 10:48:29 +0100

    Update newhello.java

--- newhello.java
+++ newhello.java
@@ -5,6 +5,7 @@
         static int ccount = 0;  
         public Robot(int xxlocation, int yylocation, String nname) {  
             xlocation = xxlocation;  
+            xlocatio = zlocation;
             ylocation = yylocation;  
             name = nname;  
             ccount++;         
