c0344c5102c679428e7b5d49ac40caea72e80c75
Abdelillah TACHTIOUI <abdelillah.tachtioui@gmail.com>
2019-03-19 10:45:08 +0100

    Create newhello.java

---
+++ newhello.java
@@ -0,0 +1,12 @@
+ public class Robot {  
+        int xlocation;  
+        int ylocation;  
+        String name;  
+        static int ccount = 0;  
+        public Robot(int xxlocation, int yylocation, String nname) {  
+            xlocation = xxlocation;  
+            ylocation = yylocation;  
+            name = nname;  
+            ccount++;         
+        } 
+  }
