1b131675c66ee6a4f12c3c5afbbb09a985638cef
Abdelillah TACHTIOUI <abdelillah.tachtioui@gmail.com>
2019-03-19 10:24:35 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld
       public static void main(String[] args)
       {       
          System.out.println("Hello alfstore @ GitHub!"); 
-         System.outt.println("Hello alfstore @ GitHub!"); 
+         System.println("Hello alfstore @ GitHub!"); 
       }
 } 
 
