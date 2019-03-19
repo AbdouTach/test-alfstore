adfa5eb23650643dcf9728f12bf2e10bcc66815a
Abdelillah TACHTIOUI <abdelillah.tachtioui@gmail.com>
2019-03-19 10:26:57 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,17 @@ public class HelloWorld
 {   
       public static void main(String[] args)
       {       
-         System.out.println("Hello alfstore @ GitHub!"); 
-         System.println("Hello alfstore @ GitHub!"); 
+         if (solidom.equalsIgnoreCase("esfera"){
+        vol=(4.0/3)*Math.pi*Math.pow(raiom,3);
+    }
+    else {
+        if (solidom.equalsIgnoreCase("cilindro") {
+            vol=Math.pi*Math.pow(raiom,2)*alturam;
+        }
+        else {
+            vol=(1.0/3)*Math.pi*Math.pow(raiom,2)*alturam;
+        }
+    } 
       }
 } 
 
